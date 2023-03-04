7c46ea1d02410a82b81ca3574f9847c5f0b75743
triet <pvmtriet232@gmail.com>
2023-03-04 14:37:44 +0700

    exexuting SQL query on db

--- pkg/mysql/snippets.go
+++ pkg/mysql/snippets.go
@@ -11,7 +11,29 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	// Write the SQL statement we want to execute. I've split it over two lines
+	// for readability (which is why it's surrounded with backquotes instead
+	// of normal double quotes).
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+	// Use the Exec() method on the embedded connection pool to execute the
+	// statement. The first parameter is the SQL statement, followed by the
+	// title, content and expiry values for the placeholder parameters. This
+	// method returns a sql.Result object, which contains some basic
+	// information about what happened when the statement was executed.
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+	// Use the LastInsertId() method on the result object to get the ID of our
+	// newly inserted record in the snippets table.
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	// The ID returned has the type int64, so we convert it to an int type
+	// before returning.
+	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
